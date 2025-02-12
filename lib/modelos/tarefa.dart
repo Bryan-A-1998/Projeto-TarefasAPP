7d2aa8d13fdca951e2a1f6c1a59e984bb2bced94
Bryan Alex <106479195+Bryan-A-1998@users.noreply.github.com>
2025-02-12 00:10:04 -0300

    Add files via upload

--- lib/modelos/tarefa.dart
+++ lib/modelos/tarefa.dart
@@ -1,11 +1,37 @@
 class Tarefa {
-  String nome;
-  DateTime data;
-  String descricao;
- 
+  final int? id;
+  final String titulo;
+  final String descricao;
+  final DateTime dataHora;
+  final String? foto; // Opcional
+
   Tarefa({
-    required this.nome,
-    required this.data,
+    this.id,
+    required this.titulo,
     required this.descricao,
+    required this.dataHora,
+    this.foto,
   });
+
+  // Converte JSON para Objeto Tarefa
+  factory Tarefa.fromJson(Map<String, dynamic> json) {
+    return Tarefa(
+      id: json['id'],
+      titulo: json['titulo'],
+      descricao: json['descricao'],
+      dataHora: DateTime.parse(json['horario']), 
+      foto: json['foto'] ?? null, 
+    );
+  }
+
+  // Converte Objeto Tarefa para JSON (usado ao enviar requisições)
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'titulo': titulo,
+      'descricao': descricao,
+      'horario': dataHora.toIso8601String(),
+      'foto': foto,
+    };
+  }
 }
